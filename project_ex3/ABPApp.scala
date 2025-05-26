7081ac724031f6bd82d0707025a0c832d167a35e
André <andre.adrego@gmail.com>
2025-05-26 12:58:32 +0100

    ServerState com windows (usa wsl) ABPApp é ex3 build.sbt é para o ex3 Correr `sbt run` em pasta project_ex3

---
+++ project_ex3/ABPApp.scala
@@ -0,0 +1,145 @@
+package project_ex3
+
+import akka.actor._
+import scala.util.Random
+import scala.concurrent.duration._
+import akka.event.Logging
+
+case class Message(bit: Int, content: String)
+case class Ack(bit: Int)
+case object Start
+case object TriggerNext
+
+//  Trans
+class Trans(receiver: ActorRef, sender: ActorRef, correct: Boolean, failiure: Boolean) extends Actor {
+  val log = Logging (context.system, this)
+  def receive: Receive = {
+    case msg: Message =>
+
+      if (correct){ // envia sempre corretamente
+        log.info(s"[Trans] Enviando: ${msg.content} (bit=${msg.bit})")
+        receiver ! msg
+
+      } else if (failiure){
+        // TODO: implement failiure
+
+      } else {
+        if (Random.nextBoolean()) {
+          log.info(s"[Trans] Enviando: ${msg.content} (bit=${msg.bit})")
+          receiver ! msg
+          while (Random.nextBoolean()) receiver ! msg // duplicação de menssagem
+        } else {
+          log.info(s"[Trans] PERDEU mensagem: ${msg.content}")
+          sender ! TriggerNext // sender tem que enviar de novo
+        }
+      }
+  }
+}
+
+//  AckChannel
+class AckChannel(sender: ActorRef, receiver: ActorRef, correct: Boolean, failiure: Boolean) extends Actor {
+  val log = Logging (context.system, this)
+  def receive: Receive = {
+    case ack: Ack =>
+
+      if (correct){
+        log.info(s"[AckChannel] Enviando Ack(${ack.bit})")
+        sender ! ack
+
+      } else if (failiure){
+        //TODO: implementfailiure
+
+      } else {
+        if (Random.nextBoolean()) {
+          log.info(s"[AckChannel] Enviando Ack(${ack.bit})")
+          sender ! ack
+          while (Random.nextBoolean()) sender ! ack // duplicação de ack
+        } else {
+          log.info(s"[AckChannel] PERDEU Ack(${ack.bit})")
+          receiver ! ack
+        }
+      }
+
+  }
+}
+
+//  Receiver
+class Receiver(getSender: () => ActorRef, correct: Boolean, failiure: Boolean) extends Actor {
+  val log = Logging (context.system, this)
+  val ackChannel = context.actorOf(Props(new AckChannel(getSender(), this.self, correct, failiure)), "ack-channel")
+  var expectedBit = 0
+
+  def receive: Receive = {
+    case Message(bit, content) =>
+      if (bit == expectedBit) {
+        log.info(s"[Receiver] Entregou: '$content' com bit $bit")
+        expectedBit = 1 - expectedBit
+      } else {
+        log.info(s"[Receiver] Ignorou duplicado com bit $bit")
+      }
+      ackChannel ! Ack(bit)
+    case Ack(bit) =>
+      log.info(s"[Receiver] Reenvia ack: com bit $bit")
+      ackChannel ! Ack(bit)
+  }
+}
+
+//  Sender
+class Sender(receiver: ActorRef, correct: Boolean, failiure: Boolean) extends Actor {
+  val log = Logging (context.system, this)
+  val trans = context.actorOf(Props(new Trans(receiver, this.self, correct, failiure)), "trans")
+  var bit = 0
+  var messages = List("msg1", "msg2", "msg3")
+
+  def receive: Receive = {
+    case Start =>
+      self ! TriggerNext
+
+    case TriggerNext =>
+      if (messages.nonEmpty) {
+        val msg = messages.head
+        log.info(s"[Sender] A enviar '$msg' com bit $bit")
+        trans ! Message(bit, msg)
+      }
+
+    case Ack(receivedBit) => // TODO: melhorar a terminação, acho que n está muioto bem.....
+      if (messages.isEmpty) context.system.terminate()
+
+      if (receivedBit == bit) {
+        log.info(s"[Sender] Recebeu Ack correto: $receivedBit")
+        bit = 1 - bit
+        messages = messages.tail
+        if (messages.isEmpty) context.system.terminate()
+        self ! TriggerNext
+      } else {
+        log.info(s"[Sender] Ignorou Ack errado: $receivedBit")
+      }
+  }
+}
+
+//  App
+object ABPApp extends App {
+  lazy val system = ActorSystem("ABPSystem")
+
+  val correct = true // true para tudo certo
+  val failiure= false // TODO: o failiure de momemnto n faz nada ,logo tem que estar a falso
+
+  var senders: ActorRef = null
+
+  val receiver = system.actorOf(Props(new Receiver(() => senders, correct, failiure)), "receiver")
+
+  senders = system.actorOf(Props(new Sender(receiver, correct, failiure)), "sender")
+
+  senders ! Start
+
+}
+
+/*
+A hieraquia que existe:
+
+         "ABPSystem"
+        /           \
+    "Sender"      "Receiver"
+        |              |
+    "Trans"        "Ack_channel"
+*/
